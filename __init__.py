a08d98d8e1f632d6d09c07903f8bd3a9b11701d4
Jedd Bellamy-Carter <jeddidiah.bellamy-carter@nottingham.ac.uk>
2020-06-01 16:12:13 +0100

    Update __init__.py version

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 #-*- coding:utf-8 -*-
 
 __title__ = 'chargePlacer'
-__version__ = '0.1.0'
+__version__ = '0.9.0'
 __author__ = 'Jedd Bellamy-Carter'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2020, Jedd Bellamy-Carter, University of Nottingham'
